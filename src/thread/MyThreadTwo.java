82da5eb0537baa9c5dbc1d19a005c4ff8fa3a12d
kungreat <mydreams2018@outlook.com>
2020-05-07 00:12:42 +0800

    java多线程synchronized注意点

--- src/thread/MyThreadTwo.java
+++ src/thread/MyThreadTwo.java
@@ -2,9 +2,7 @@ package thread;
 
 public class MyThreadTwo implements Runnable{
 
-	public static int num=0;
 	private static String ALOCK = "A";
-	private static String BLOCK = "B";
 	@Override
 	public void run() {
 //		System.out.println(Thread.currentThread().getName()+"waitLock");
@@ -17,30 +15,17 @@ public class MyThreadTwo implements Runnable{
 	}
 
 	static void addNumA() throws Exception {
-		 Thread thread = new Thread() {
-
-			@Override
-			public void run() {
-				for(int x=0;x<10;x++) {
-					 System.out.println(x);
-				}
-			}
-			 
-		 };
-		 thread.start();
-		 System.out.println(Thread.interrupted());//测试当前线程是否已被中断。会清除中断标识
+//		 System.out.println(Thread.interrupted());//测试当前线程是否已被中断。会清除中断标识
 //		 System.out.println(Thread.currentThread().isInterrupted());//测试当前线程是否已被中断
 //		 Thread.sleep(100);
-//		 synchronized(ALOCK) {
-//			 for(int x=0;x<1000;x++) {
-////				 if(Thread.currentThread().isInterrupted() && x > 5) {
-////					 break;
-////				 }
-//				
-//			 }
-//			 
-//		 }
-		 thread.join();
+		 synchronized(ALOCK) {
+//			 Thread.sleep(100);
+			 ALOCK = "B";
+			 for(int x=0;x<6;x++) {
+				 Thread.sleep(1000);
+				 System.out.println(Thread.currentThread().getName()+":"+x);
+			 }
+		 }
 //		 System.out.println(Thread.holdsLock(ALOCK));//当且仅当当前线程在指定对象上持有监视器锁时才返回true。
 	}
 
--- src/thread/TestOne.java
+++ src/thread/TestOne.java
@@ -4,11 +4,12 @@ public class TestOne {
 
 	public static void main(String[] args) throws Exception {
 		MyThreadTwo two = new MyThreadTwo();
-		MyThreadGroup mg = new MyThreadGroup("custom");
-		Thread thread1 = new Thread(mg,two);
+		Thread thread1 = new Thread(two);
+		Thread thread2 = new Thread(two);
 		thread1.start();
-		thread1.interrupt();//中断此线程,必须在线程启动后才启作用
-		thread1.join();
+		thread2.start();
+//		thread1.interrupt();//中断此线程,必须在线程启动后才启作用
+//		thread1.join();
 	}
 
 }
