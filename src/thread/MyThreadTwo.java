a1c5d755712893abf1c1bb071d4f59ab5ff74bc6
kungreat <mydreams2018@outlook.com>
2020-05-06 23:01:16 +0800

    interrupte中断标识

--- src/thread/MyThreadTwo.java
+++ src/thread/MyThreadTwo.java
@@ -17,9 +17,31 @@ public class MyThreadTwo implements Runnable{
 	}
 
 	static void addNumA() throws Exception {
-		 synchronized(ALOCK) {
-			Thread.sleep(2000);
-		 }
+		 Thread thread = new Thread() {
+
+			@Override
+			public void run() {
+				for(int x=0;x<10;x++) {
+					 System.out.println(x);
+				}
+			}
+			 
+		 };
+		 thread.start();
+		 System.out.println(Thread.interrupted());//测试当前线程是否已被中断。会清除中断标识
+//		 System.out.println(Thread.currentThread().isInterrupted());//测试当前线程是否已被中断
+//		 Thread.sleep(100);
+//		 synchronized(ALOCK) {
+//			 for(int x=0;x<1000;x++) {
+////				 if(Thread.currentThread().isInterrupted() && x > 5) {
+////					 break;
+////				 }
+//				
+//			 }
+//			 
+//		 }
+		 thread.join();
+//		 System.out.println(Thread.holdsLock(ALOCK));//当且仅当当前线程在指定对象上持有监视器锁时才返回true。
 	}
 
 	
--- src/thread/TestOne.java
+++ src/thread/TestOne.java
@@ -7,7 +7,8 @@ public class TestOne {
 		MyThreadGroup mg = new MyThreadGroup("custom");
 		Thread thread1 = new Thread(mg,two);
 		thread1.start();
-		Thread.sleep(1000);
+		thread1.interrupt();//中断此线程,必须在线程启动后才启作用
+		thread1.join();
 	}
 
 }
