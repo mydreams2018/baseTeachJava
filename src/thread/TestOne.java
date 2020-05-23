860cb891b3cfe521173bdf3e46c51da938998e8f
kungreat <mydreams2018@outlook.com>
2020-05-23 18:08:28 +0800

    ThreadLocal

--- src/thread/TestOne.java
+++ src/thread/TestOne.java
@@ -1,13 +1,28 @@
 package thread;
 
+import java.util.function.Supplier;
+
 public class TestOne {
-	public volatile static boolean isDone = true;
-	
+  
+//	public static final ThreadLocal TH2 = new ThreadLocal();
 	public static void main(String[] args) throws Exception {
-		VolatileTest ts = new VolatileTest();
-		Thread thread = new Thread(ts);
-		thread.start();
-		ts.eat();
+		/*
+		 * TH.set("main"); TH.set("main1"); TH2.set("main2"); VolatileTest ts = new
+		 * VolatileTest(); Thread thread = new Thread(ts); thread.start();
+		 * Thread.sleep(1000); TH2.remove();
+		 * System.out.println(Thread.currentThread().getName()+":"+TH.get());
+		 * System.out.println(Thread.currentThread().getName()+":"+TH2.get());
+		 */
+		ThreadLocal TH = null ;
+		for(int x=0;x <5;x++) {
+			TH = new ThreadLocal();
+			TH.set(x);
+			Thread.sleep(200);
+		}
+		TH.remove();
+		System.gc();
+		Thread currentThread = Thread.currentThread();
+		System.out.println(currentThread);
 	}
 
 }
--- src/thread/VolatileTest.java
+++ src/thread/VolatileTest.java
@@ -1,38 +1,16 @@
 package thread;
 
 public class VolatileTest implements Runnable{
-	private Integer food;
-	private boolean around = false;
 	
 	@Override
 	public void run() {
 		try {
-			for(int x=0;x<10;x++) {
-				synchronized (this) {
-					while(around) {
-						this.wait();
-					}
-					food=x;
-					around = true;
-					System.out.println(Thread.currentThread().getName()+":生产"+food);
-					this.notify();
-				}
-			}
+//			System.out.println(Thread.currentThread().getName()+":"+TestOne.TH2.get());
+//			System.out.println(Thread.currentThread().getName()+":"+TestOne.TH.get());
 		}catch (Exception e) {
 			e.printStackTrace();
 		}
 		
 	}
-	public void eat() throws Exception {
-		for(int x=0;x<10;x++) {
-			synchronized (this) {
-				while(!around) {
-					this.wait();
-				}
-				System.out.println(Thread.currentThread().getName()+":消费"+food);
-				around = false;
-				this.notify();
-			}
-		}
-	}
+
 }
