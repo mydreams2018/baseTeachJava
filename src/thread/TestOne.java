e2fb9613f2908e512c49cc6c46e6fa279c3d6ea7
kungreat <mydreams2018@outlook.com>
2020-06-01 23:25:42 +0800

    Lock锁注意点

--- src/thread/TestOne.java
+++ src/thread/TestOne.java
@@ -1,28 +1,14 @@
 package thread;
 
-import java.util.function.Supplier;
 
 public class TestOne {
-  
-//	public static final ThreadLocal TH2 = new ThreadLocal();
+
 	public static void main(String[] args) throws Exception {
-		/*
-		 * TH.set("main"); TH.set("main1"); TH2.set("main2"); VolatileTest ts = new
-		 * VolatileTest(); Thread thread = new Thread(ts); thread.start();
-		 * Thread.sleep(1000); TH2.remove();
-		 * System.out.println(Thread.currentThread().getName()+":"+TH.get());
-		 * System.out.println(Thread.currentThread().getName()+":"+TH2.get());
-		 */
-		ThreadLocal TH = null ;
-		for(int x=0;x <5;x++) {
-			TH = new ThreadLocal();
-			TH.set(x);
-			Thread.sleep(200);
-		}
-		TH.remove();
-		System.gc();
-		Thread currentThread = Thread.currentThread();
-		System.out.println(currentThread);
+		VolatileTest ts = new VolatileTest();
+		Thread thread = new Thread(ts);
+		thread.start();
+		Thread thread1 = new Thread(ts);
+		thread1.start();
 	}
 
 }
--- src/thread/VolatileTest.java
+++ src/thread/VolatileTest.java
@@ -1,16 +1,29 @@
 package thread;
 
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
+
 public class VolatileTest implements Runnable{
 	
+	private Lock lock = new ReentrantLock();
+
 	@Override
 	public void run() {
 		try {
-//			System.out.println(Thread.currentThread().getName()+":"+TestOne.TH2.get());
-//			System.out.println(Thread.currentThread().getName()+":"+TestOne.TH.get());
-		}catch (Exception e) {
+			lock.lock();
+			System.out.println(Thread.currentThread().getName());
+			Thread.sleep(2000);
+		} catch (InterruptedException e) {
 			e.printStackTrace();
+		}finally {
+			try {
+				lock.unlock();
+			}catch (Exception e) {
+				
+			}
 		}
-		
+		System.out.println("edddddddddddddddddddddddnd");
+		System.out.println("edddddddddddddddddddddddddddddddnd");
 	}
 
 }
