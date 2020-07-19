3cc85f585bc4456f6ff07be83c598833a0508bba
kungreat <mydreams2018@outlook.com>
2020-07-19 12:54:17 +0800

    LinkedBlockingDeque-2

--- src/juc/JucTest.java
+++ src/juc/JucTest.java
@@ -3,16 +3,16 @@ package juc;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.concurrent.ArrayBlockingQueue;
+import java.util.concurrent.LinkedBlockingDeque;
 import java.util.function.Predicate;
 
 
 public class JucTest {
-	static final ArrayBlockingQueue<String> AQ = new ArrayBlockingQueue<>(2);
+	static final LinkedBlockingDeque<String> AQ = new LinkedBlockingDeque<>(2);
 	public static void main(String[] args) throws Exception {
-		AQ.add("1");
 		AQ.add("2");
-		
-		System.out.println(AQ.poll());
+		AQ.addFirst("1");
+		System.out.println(AQ.pop());
 		System.out.println(AQ.poll());
 		System.out.println(AQ.take());
 	}
