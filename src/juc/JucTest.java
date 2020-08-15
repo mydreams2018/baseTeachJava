cd0d36e159f6859ac7651c4a37327a750fa4fbfa
kungreat <mydreams2018@outlook.com>
2020-08-15 23:21:09 +0800

    base

--- src/juc/JucTest.java
+++ src/juc/JucTest.java
@@ -1,26 +1,47 @@
 package juc;
 
-import java.util.concurrent.LinkedTransferQueue;
 import java.util.concurrent.SynchronousQueue;
 
 public class JucTest {
-	static final SynchronousQueue<String> AQ = new SynchronousQueue<>();
+	static final SynchronousQueue<String> AQ = new SynchronousQueue<>(true);
 	public static void main(String[] args) throws Exception {
 		new Thread() {
-
 			@Override
 			public void run() {
 				try {
-					Thread.sleep(200);
-					AQ.add("1");
+					Thread.sleep(150);
+					AQ.put("3");
 				} catch (InterruptedException e) {
 					e.printStackTrace();
 				}
 			}
 			
 		}.start();
-		System.out.println(AQ.take());
-//		AQ.put("4");
+		new Thread() {
+			@Override
+			public void run() {
+				try {
+					Thread.sleep(100);
+					AQ.put("2");
+				} catch (InterruptedException e) {
+					e.printStackTrace();
+				}
+			}
+			
+		}.start();
+		new Thread() {
+			@Override
+			public void run() {
+				try {
+					Thread.sleep(200);
+					System.out.println(AQ.take());
+				} catch (InterruptedException e) {
+					e.printStackTrace();
+				}
+			}
+			
+		}.start();
+		AQ.put("1");
 //		System.out.println(AQ.take());
 //		System.out.println(AQ.take());
 //		System.out.println(AQ.take());
