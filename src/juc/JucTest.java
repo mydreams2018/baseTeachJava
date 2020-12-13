3e332431cafa04e9ababcbc8c92ad8ec3036ec0d
kungreat <mydreams2018@outlook.com>
2020-12-13 16:42:19 +0800

    CountDownLatch

--- src/juc/JucTest.java
+++ src/juc/JucTest.java
@@ -5,18 +5,18 @@ import java.util.Iterator;
 import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.ConcurrentSkipListMap;
 import java.util.concurrent.CopyOnWriteArrayList;
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.TimeUnit;
 
 public class JucTest {
+	static CountDownLatch countDown = new CountDownLatch(1);
 	static ConcurrentHashMap<String,Integer> cur;
 	public static void main(String[] args) throws Exception {
 		cur = new ConcurrentHashMap<>();
 		cur.put("one",1);
 		ConcurrentHashMapTest concurrentHashMapTest = new ConcurrentHashMapTest();
 		concurrentHashMapTest.start();
-		ConcurrentHashMapTest concurrentHashMapTest2 = new ConcurrentHashMapTest();
-		concurrentHashMapTest2.start();
-		concurrentHashMapTest.join();
-		concurrentHashMapTest2.join();
+		countDown.await(2,TimeUnit.SECONDS);
 		System.out.println(cur.get("one"));
 	}
 	
@@ -24,11 +24,14 @@ public class JucTest {
 
 		@Override
 		public void run() {
-			for(int x=0;x<10000;x++) {
-				Integer integer = cur.get("one");
-				integer++;
-				cur.put("one", integer);
+			try {
+				Thread.sleep(3000);
+				cur.put("one",3000);
+			} catch (InterruptedException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
 			}
+			countDown.countDown();
 		}
 		
 	}
