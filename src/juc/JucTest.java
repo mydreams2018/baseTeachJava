3890a00b64612a8a332c7e033519c45f96b48bc9
kungreat <mydreams2018@outlook.com>
2021-02-13 11:49:31 +0800

    ThreadPoolExecutor-one

--- src/juc/JucTest.java
+++ src/juc/JucTest.java
@@ -1,33 +1,26 @@
 package juc;
 
-import java.util.ArrayList;
-import java.util.Iterator;
-import java.util.concurrent.ConcurrentHashMap;
-import java.util.concurrent.ConcurrentSkipListMap;
-import java.util.concurrent.CopyOnWriteArrayList;
-import java.util.concurrent.CountDownLatch;
-import java.util.concurrent.CyclicBarrier;
-import java.util.concurrent.Phaser;
-import java.util.concurrent.Semaphore;
-import java.util.concurrent.TimeUnit;
+import java.util.concurrent.*;
 
 public class JucTest {
-	static MyPhaser semaphore = new MyPhaser(5);
+	static ThreadPoolExecutor threadPoolExecutor = new ThreadPoolExecutor(2,5,10L,
+			TimeUnit.SECONDS,new ArrayBlockingQueue<Runnable>(8));
 
 	public static void main(String[] args) throws Exception {
-		for(int x=0;x<6;x++) {
-			new Thread(new Runnable() {
-				@Override 
+		for(int x=0;x<1;x++){
+			threadPoolExecutor.execute(new Runnable() {
+				@Override
 				public void run() {
-					System.out.println(semaphore.arriveAndAwaitAdvance());
+					try {
+						Thread.sleep(30000);
+					} catch (InterruptedException e) {
+						e.printStackTrace();
+					}
+					System.out.println(Thread.currentThread().getName());
 				}
-			}).start();
+			});
 		}
-//327681 327680 4295294981
-//		System.out.println(327681 & 0xffff);
-//		System.out.println(327680 & 0xffff);
-//		int ct =(int)4295294981L;
-//		System.out.println(ct & 0xffff);
+		System.out.println("main-end");
 	}
 
 }
