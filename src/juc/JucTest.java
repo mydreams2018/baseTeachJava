34b5aa1804f7bc9221580d291295ae3b5c0fbf91
kungreat <mydreams2018@outlook.com>
2021-02-14 11:16:20 +0800

    base

--- src/juc/JucTest.java
+++ src/juc/JucTest.java
@@ -7,20 +7,14 @@ public class JucTest {
 			TimeUnit.SECONDS,new ArrayBlockingQueue<>(8));
 
 	public static void main(String[] args) throws Exception {
-		for(int x=0;x<13;x++){
-			threadPoolExecutor.execute(new Runnable() {
-				@Override
-				public void run() {
-					try {
-						Thread.sleep(50000);
-					} catch (InterruptedException e){
-						e.printStackTrace();
-					}
-					System.out.println(Thread.currentThread().getName());
-				}
-			});
-		}
 		System.out.println(threadPoolExecutor.allowsCoreThreadTimeOut());
+		threadPoolExecutor.execute(new Runnable() {
+			@Override
+			public void run() {
+				System.out.println(Thread.currentThread().getName());
+			}
+		});
+		System.out.println("end");
 	}
 
 }
