577949885a961bc0f9b7490e4a6df80598c974a6
kungreat <mydreams2018@outlook.com>
2021-02-14 17:55:11 +0800

    ThreadPoolExecutor

--- src/juc/JucTest.java
+++ src/juc/JucTest.java
@@ -3,18 +3,26 @@ package juc;
 import java.util.concurrent.*;
 
 public class JucTest {
-	final static ThreadPoolExecutor threadPoolExecutor = new ThreadPoolExecutor(2,5,10L,
-			TimeUnit.SECONDS,new ArrayBlockingQueue<>(8));
+	final static ThreadPoolExecutor threadPoolExecutor = new ThreadPoolExecutor(2,5,2L,
+			TimeUnit.SECONDS,new ArrayBlockingQueue<>(8),new MyRejectedExecutionHandler());
 
 	public static void main(String[] args) throws Exception {
+		for(int x=0;x<14;x++){
+			threadPoolExecutor.execute(new Runnable() {
+				@Override
+				public void run() {
+					try {
+						Thread.sleep(200);
+					} catch (InterruptedException e) {
+						e.printStackTrace();
+					}
+					System.out.println(Thread.currentThread().getName());
+				}
+			});
+		}
+//		threadPoolExecutor.allowCoreThreadTimeOut(true);
+		threadPoolExecutor.shutdown();
 		System.out.println(threadPoolExecutor.allowsCoreThreadTimeOut());
-		threadPoolExecutor.execute(new Runnable() {
-			@Override
-			public void run() {
-				System.out.println(Thread.currentThread().getName());
-			}
-		});
-		System.out.println("end");
 	}
 
 }
