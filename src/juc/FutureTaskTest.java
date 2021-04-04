748bd5517b7ffaa3ecfdeedaebbc4fdef672a796
kungreat <mydreams2018@outlook.com>
2021-04-04 18:46:14 +0800

    ForkJoinPool

--- src/juc/FutureTaskTest.java
+++ src/juc/FutureTaskTest.java
@@ -3,17 +3,49 @@ package juc;
 import java.util.concurrent.*;
 
 public class FutureTaskTest {
-    static ScheduledThreadPoolExecutor futureTask = new ScheduledThreadPoolExecutor(2);
+    static ForkJoinPool futureTask = new ForkJoinPool();
 
     public static void main(String s[]) throws Exception{
-        Future<?> schedule = futureTask.submit(new Runnable() {
+        Long invoke = futureTask.invoke(new ForkJoinTask<Long>() {
+            private Long rt = 0L;
             @Override
-            public void run() {
-                System.out.println(Thread.currentThread().getName());
+            public Long getRawResult() {
+                return 55L+rt;
             }
-        },"test");
-        Object o = schedule.get();
-        System.out.println(o);
+
+            @Override
+            protected void setRawResult(Long value) {
+            }
+
+            @Override
+            protected boolean exec() {
+                System.out.println(Thread.currentThread().getName());
+                ForkJoinTask<Long> task = new ForkJoinTask<>() {
+                    @Override
+                    public Long getRawResult() {
+                        return 55L;
+                    }
+                    @Override
+                    protected void setRawResult(Long value) {
+                    }
+                    @Override
+                    protected boolean exec() {
+                        System.out.println(Thread.currentThread().getName());
+                        return true;
+                    }
+                };
+                task.fork();
+                try {
+                    Thread.sleep(500);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+                rt = task.join();
+                return true;
+            }
+        });
+//        Object o = schedule.get();
+        System.out.println(invoke);
         // 100 2  period>0  102 104 106 108   [6]
         //100 2  period<0   102 108+2
     }
