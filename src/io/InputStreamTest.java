18100a90e2da988cada4e21c89caf135644c5b1c
kungreat <mydreams2018@outlook.com>
2021-06-06 12:00:20 +0800

    CheckedInputStream

--- src/io/InputStreamTest.java
+++ src/io/InputStreamTest.java
@@ -3,26 +3,23 @@ package io;
 import java.io.*;
 import java.nio.charset.Charset;
 import java.util.Random;
+import java.util.zip.Adler32;
+import java.util.zip.CheckedInputStream;
+import java.util.zip.Checksum;
 
 public class InputStreamTest {
     static File file = new File("D:\\temp\\src\\test.txt");
 //    static PipedInputStream pipedInputStream = new PipedInputStream();
 //    static PipedOutputStream pipedOutputStream = new PipedOutputStream();
     public static void main(String s[]) throws Exception {
-        try(FileInputStream fileInputStream = new FileInputStream(file);//0-10000
-            BufferedInputStream bufferedInputStream = new BufferedInputStream(fileInputStream)){
-            System.out.println(bufferedInputStream.read());
-            System.out.println(bufferedInputStream.read());
-            bufferedInputStream.mark(20000);
-            System.out.println(bufferedInputStream.read());
-            System.out.println(bufferedInputStream.read());
-            System.out.println(bufferedInputStream.read());
-            bufferedInputStream.reset();
-            System.out.println(bufferedInputStream.read());
-            System.out.println(bufferedInputStream.read());
-            System.out.println(bufferedInputStream.read());
-        }catch (Exception e){
-            e.printStackTrace();
+        Checksum checksum = new Adler32();
+        byte[] bt = {97,115,97,115,97,115,97,115};
+        checksum.update(bt);
+        System.out.println(checksum.getValue());//230425357
+        try(FileInputStream fileInputStream = new FileInputStream(file);
+            CheckedInputStream checkedInputStream = new CheckedInputStream(fileInputStream,new Adler32())){
+            byte[] bytes = checkedInputStream.readAllBytes();
+            System.out.println(checkedInputStream.getChecksum().getValue());
         }
     }
 }
---
+++ src/utils/CutoverBytes.java
@@ -0,0 +1,70 @@
+package utils;
+
+import java.util.Arrays;
+
+public class CutoverBytes {
+
+    /***指定byte数组
+     * @param t1 要合并的数组
+     * @param t2 要合并的数组
+     * @return 合并后的新数组
+     */
+    public static byte[] concat(byte[] t1,byte[] t2){
+        byte[] result = new byte[t1.length+t2.length];
+        System.arraycopy(t1,0,result,0,t1.length);
+        System.arraycopy(t2,0,result,t1.length,t2.length);
+        return result;
+    }
+    /***泛型数组
+     * @param t1 要合并的数组
+     * @param t2 要合并的数组
+     * @return 合并后的新数组
+     */
+    public static <T> T[] concat(T[] t1,T[] t2){
+        T[] result = Arrays.copyOf(t1,t1.length + t2.length);
+        System.arraycopy(t2,0,result,t1.length,t2.length);
+        return result;
+    }
+    /***
+     * @param a 数值
+     * @return 把数值转换成byte数组
+     */
+    public static byte[] intToByteArray(int a) {
+        return new byte[] {
+                (byte) ((a >> 24)),
+                (byte) ((a >> 16) & 255),
+                (byte) ((a >> 8)  & 255),
+                (byte) ((a >> 0)  & 255)
+        };
+    }
+    /***
+     * @param a 数值
+     * @return 把数值转换成byte数组
+     */
+    public static byte[] longToByteArray(long a) {
+        return new byte[] {
+                (byte) ((a >> 56)),
+                (byte) ((a >> 48)  & 255),
+                (byte) ((a >> 40)  & 255),
+                (byte) ((a >> 32)  & 255),
+                (byte) ((a >> 24)  & 255),
+                (byte) ((a >> 16)  & 255),
+                (byte) ((a >> 8)   & 255),
+                (byte) ((a >> 0)   & 255)
+        };
+    }
+    /***
+     * @param bytes 数组
+     * @return 把数组转换成long数值
+     */
+    public static long readLong(byte[] bytes) {
+        return (((long)bytes[0] << 56) +
+                ((long)(bytes[1] & 255) << 48) +
+                ((long)(bytes[2] & 255) << 40) +
+                ((long)(bytes[3] & 255) << 32) +
+                ((long)(bytes[4] & 255) << 24) +
+                ((bytes[5] & 255) << 16) +
+                ((bytes[6] & 255) <<  8) +
+                ((bytes[7] & 255) <<  0));
+    }
+}
