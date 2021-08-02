3576d74b14f6cf2e8b6b34f2fb7ffc9b31fd6dbb
kungreat <mydreams2018@outlook.com>
2021-08-02 14:43:04 +0800

    ByteArrayOutputStream

--- src/io/OutputStreamTest.java
+++ src/io/OutputStreamTest.java
@@ -6,9 +6,11 @@ public class OutputStreamTest {
     final static File file = new File("D:\\temp\\src\\test.txt");
     public static void main(String st[]){
 
-        try(OutputStream outputStream = new FileOutputStream(file,true)){
-            outputStream.write(23);
-            outputStream.write(23);
+        try(OutputStream outputStream = new FileOutputStream(file)){
+            ByteArrayOutputStream byteArrayOutputStream = new ByteArrayOutputStream();
+            String s = "哈哈哈";
+            byteArrayOutputStream.write(s.getBytes());
+            byteArrayOutputStream.writeTo(outputStream);
             outputStream.flush();
         }catch (Exception e){
             e.printStackTrace();
