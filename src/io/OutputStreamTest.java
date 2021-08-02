6fa53fc170117ebada99aeb84fbeb591a1459889
kungreat <mydreams2018@outlook.com>
2021-08-02 16:06:41 +0800

    BufferedOutputStream

--- src/io/OutputStreamTest.java
+++ src/io/OutputStreamTest.java
@@ -6,12 +6,10 @@ public class OutputStreamTest {
     final static File file = new File("D:\\temp\\src\\test.txt");
     public static void main(String st[]){
 
-        try(OutputStream outputStream = new FileOutputStream(file)){
-            ByteArrayOutputStream byteArrayOutputStream = new ByteArrayOutputStream();
-            String s = "哈哈哈";
-            byteArrayOutputStream.write(s.getBytes());
-            byteArrayOutputStream.writeTo(outputStream);
-            outputStream.flush();
+        try(OutputStream outputStream = new FileOutputStream(file);
+            BufferedOutputStream bufferedOutputStream = new BufferedOutputStream(outputStream)){
+            bufferedOutputStream.write(86);
+            bufferedOutputStream.flush();
         }catch (Exception e){
             e.printStackTrace();
         }
