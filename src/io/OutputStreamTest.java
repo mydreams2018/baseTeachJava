e89a03ffee85828b6ffffe3ae92babc107deb432
kungreat <mydreams2018@outlook.com>
2021-08-03 18:17:55 +0800

    InputStreamReader

--- src/io/OutputStreamTest.java
+++ src/io/OutputStreamTest.java
@@ -1,17 +1,28 @@
 package io;
 
 import java.io.*;
+import java.nio.charset.Charset;
 
 public class OutputStreamTest {
     final static File file = new File("D:\\temp\\src\\test.txt");
     public static void main(String st[]){
 
-        try(OutputStream outputStream = new FileOutputStream(file);
-            BufferedOutputStream bufferedOutputStream = new BufferedOutputStream(outputStream)){
-            bufferedOutputStream.write(86);
-            bufferedOutputStream.flush();
+        try(InputStream inputStream = new FileInputStream(file);
+            InputStreamReader inputStreamReader = new InputStreamReader(inputStream)
+        ){
+//            while(inputStreamReader.ready()){
+//                System.out.print((char)inputStreamReader.read());
+//            }
+            char ca[] = new char[100];
+            int num = inputStreamReader.read(ca);
+            for(int x=0;x<num;x++){
+                System.out.print(ca[x]);
+            }
+
         }catch (Exception e){
             e.printStackTrace();
         }
+        // 11100110 10001000 10010001
+          // 0110001000010001
     }
 }
