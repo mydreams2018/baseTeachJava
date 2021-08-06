557a98e6064eb0a73d99779c6f42b4c563f95ed4
kungreat <mydreams2018@outlook.com>
2021-08-06 19:07:49 +0800

    PipedReader PipedWriter

--- src/io/OutputStreamTest.java
+++ src/io/OutputStreamTest.java
@@ -5,24 +5,58 @@ import java.nio.charset.Charset;
 
 public class OutputStreamTest {
     final static File file = new File("D:\\temp\\src\\test.txt");
-    public static void main(String st[]){
 
-        try(InputStream inputStream = new FileInputStream(file);
-            InputStreamReader inputStreamReader = new InputStreamReader(inputStream);//字节缓冲区
-            BufferedReader bufferedReader = new BufferedReader(inputStreamReader);//字符缓冲区
-            PushbackReader pushbackReader = new PushbackReader(bufferedReader,16);
-        ){
-            while(pushbackReader.ready()){
-                char read = (char) pushbackReader.read();
-                System.out.print(read);
-                if('2' == read){
-                    pushbackReader.unread(51);
-                }
-            }
-        }catch (Exception e){
+    public static void main(String st[]) {
+        PipedReader pipedReader = new PipedReader();
+        PipedWriter pipedWriter = new PipedWriter();
+        try {
+            pipedWriter.connect(pipedReader);
+        } catch (IOException e) {
             e.printStackTrace();
         }
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                int read = 0;
+                try {
+                    read = pipedReader.read();
+                    while (read != -1) {
+                        System.out.print((char) read);
+                        read = pipedReader.read();
+                    }
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }finally {
+                    try {
+                        pipedReader.close();
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                }
+                System.out.println("pipedReader - end");
+            }
+        }).start();
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                try {
+                    for (int x = 0; x < 100; x++) {
+                        pipedWriter.write(48 + x);
+                        Thread.sleep(200);
+                    }
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }finally {
+                    try {
+                        pipedWriter.close();
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                }
+            }
+        }).start();
+
         // 11100110 10001000 10010001
-          // 0110001000010001
+        // 0110001000010001
     }
 }
