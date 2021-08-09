e8b4286b9c92317e20c42aa812892142c8a063db
kungreat <mydreams2018@outlook.com>
2021-08-09 20:43:11 +0800

    PrintStream

--- src/io/OutputStreamTest.java
+++ src/io/OutputStreamTest.java
@@ -4,19 +4,11 @@ import java.io.*;
 
 public class OutputStreamTest {
     final static File file = new File("D:\\temp\\src\\test.txt");
-    final static File file1 = new File("D:\\temp\\src\\test2.txt");
     public static void main(String st[]) {
         try (FileOutputStream fileOutputStream = new FileOutputStream(file);
-            OutputStreamWriter outputStreamWriter = new OutputStreamWriter(fileOutputStream);
-             FileOutputStream fileOutputStream1 = new FileOutputStream(file1);
-             OutputStreamWriter outputStreamWriter1 = new OutputStreamWriter(fileOutputStream1);
+             PrintStream printStream = new PrintStream(fileOutputStream);
         ){
-            CharArrayWriter charArrayWriter = new CharArrayWriter();
-            charArrayWriter.write("AAA");
-            charArrayWriter.append("BBB");
-            charArrayWriter.writeTo(outputStreamWriter);
-            charArrayWriter.append("CCC");
-            charArrayWriter.writeTo(outputStreamWriter1);
+
         } catch (Exception e) {
             e.printStackTrace();
         }
