9acf0a923e796b479001057a11c86060067a9b09
kungreat <mydreams2018@outlook.com>
2021-08-15 10:17:12 +0800

    URL编解码

--- src/io/OutputStreamTest.java
+++ src/io/OutputStreamTest.java
@@ -1,24 +1,24 @@
 package io;
 
+import java.io.InputStream;
 import java.net.URI;
+import java.net.URL;
+import java.net.URLDecoder;
+import java.net.URLEncoder;
 
 public class OutputStreamTest {
 
     public static void main(String st[]) throws Exception {
-        URI uri = new URI("http://www.example.com:80/languages/java/sample/a/index.html?name=kungreat#28");
-        URI uri2 = new URI("http://www.example.com:80/languages1");
-//        System.out.println(uri.getScheme());
-//        System.out.println(uri.getAuthority());
-//        System.out.println(uri.getPort());
-//        System.out.println(uri.getPath());
-//        System.out.println(uri.getFragment());
-//        System.out.println(uri.getQuery());
-//http://www.example.com:80/languages/java/sample/demo/b/index.html
-//http://www.example.com:80/languages/java/sample/a/demo/b/index.html
-//        URI u1 = new URI("demo/b/index.html");
-//        System.out.println(u1);
-//        URI resolve = uri.resolve(u1);
-//        System.out.println(resolve);
-        System.out.println(uri2.relativize(uri));
+        String haha = URLEncoder.encode("哈哈大", "UTF-8");
+        System.out.println(URLDecoder.decode(haha, "UTF-8"));
+        URL url1 = new URL("http://127.0.0.1/hello?echo="+haha);
+        try(InputStream inputStream = url1.openStream()){
+            byte[] bytes = inputStream.readAllBytes();
+            System.out.println(new String(bytes));
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+        System.out.println((char)21704);
+        //0101 010011 001000
     }
 }
