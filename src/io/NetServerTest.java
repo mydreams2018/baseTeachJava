80dd8721e6159c0ceb18ec990f930880a5657c01
kungreat <mydreams2018@outlook.com>
2021-08-28 11:59:36 +0800

    群聊扩展了解就行

--- src/io/NetServerTest.java
+++ src/io/NetServerTest.java
@@ -7,12 +7,16 @@ import java.io.OutputStream;
 import java.net.ServerSocket;
 import java.net.Socket;
 import java.time.LocalDateTime;
+import java.util.Iterator;
+import java.util.LinkedList;
+import java.util.List;
 import java.util.concurrent.ArrayBlockingQueue;
 import java.util.concurrent.ThreadPoolExecutor;
 import java.util.concurrent.TimeUnit;
 
 public class NetServerTest extends Thread implements Runnable{
 
+    private static final List<Socket> SOCKET_LIST = new LinkedList<>();
     private static final ThreadPoolExecutor THREAD_POOL_EXECUTOR = new ThreadPoolExecutor(2,30,300, TimeUnit.SECONDS,
                 new ArrayBlockingQueue<>(1024));
     private Socket accept = null;
@@ -26,21 +30,16 @@ public class NetServerTest extends Thread implements Runnable{
             System.out.println(Thread.currentThread().getName()+":get-accept");
             StringBuilder stringBuilder = new StringBuilder();
             InputStream inputStream = accept.getInputStream();
-            int read = inputStream.read();
-            while(read!=-1){
-                stringBuilder.append((char)read);
-                read = inputStream.read();
-            }
-            System.out.println(stringBuilder);
-            if(stringBuilder.toString().contains(":end")){
-                OutputStream outputStream = accept.getOutputStream();
-                String curDate = LocalDateTime.now().toString();
-                outputStream.write(curDate.getBytes());
-                outputStream.flush();
-            }else{
-                OutputStream outputStream = accept.getOutputStream();
-                outputStream.write("check-error".getBytes());
-                outputStream.flush();
+
+            while(true){
+                int read = inputStream.read();
+                while(read!=-1 && read!=10 && read!=13){
+                    stringBuilder.append((char)read);
+                    read = inputStream.read();
+                }
+                System.out.println(stringBuilder);
+                sendGroupMessage(stringBuilder.toString());
+                stringBuilder.delete(0,stringBuilder.length());
             }
         }catch (Exception e){
             e.printStackTrace();
@@ -48,12 +47,35 @@ public class NetServerTest extends Thread implements Runnable{
             if(accept != null){
                 try {
                     accept.close();
+                    SOCKET_LIST.remove(accept);
                 } catch (IOException e) {
                     e.printStackTrace();
                 }
             }
         }
         System.out.println(Thread.currentThread().getName()+":end-accept");
+    }
+
+    private void sendGroupMessage(String st) {
+        if(st == "\r" || st == "\n"){
+            return;
+        }
+        Iterator<Socket> iterator = SOCKET_LIST.iterator();
+        while(iterator.hasNext()){
+            Socket next = iterator.next();
+            if(next != accept){
+                try {
+                    OutputStream outputStream = next.getOutputStream();
+                    outputStream.write(st.getBytes());
+                    outputStream.write(System.lineSeparator().getBytes());
+                    outputStream.flush();
+                } catch (Exception e) {
+                    e.printStackTrace();
+                    iterator.remove();
+                }
+            }
+        }
+
     }
 
     public static void main(String[] args) {
@@ -63,6 +85,7 @@ public class NetServerTest extends Thread implements Runnable{
                     Socket accept = serverSocket.accept();
                     NetServerTest serverTest = new NetServerTest(accept);
                     THREAD_POOL_EXECUTOR.execute(serverTest);
+                    SOCKET_LIST.add(accept);
                 }catch (Exception e){
                     e.printStackTrace();
                 }
--- src/io/NetTest.java
+++ src/io/NetTest.java
@@ -12,20 +12,40 @@ public class NetTest {
 
     public static void main(String[] args){
         try(Socket socket = new Socket(InetAddress.getLocalHost(),52666)){
-            OutputStream outputStream = socket.getOutputStream();
-            for(int x=0;x<5;x++){
-                String rt = "dfsdfsdf122312312:end";
-                outputStream.write(rt.getBytes());
-                outputStream.flush();
-            }
-            socket.shutdownOutput();
+            //输入流
+            new Thread(new Runnable() {
+                @Override
+                public void run() {
+                    StringBuilder stringBuilder = new StringBuilder();
+                    try{
+                        InputStream inputStream = socket.getInputStream();
+                        while(true){
+                            int read = inputStream.read();
+                            while(read != -1 && read!=10 && read!=13){
+                                stringBuilder.append((char)read);
+                                read = inputStream.read();
+                            }
+                            System.out.println(stringBuilder);
+                            stringBuilder.delete(0,stringBuilder.length());
+                        }
+                    }catch (Exception e){
+                        e.printStackTrace();
+                    }
+                }
+            }).start();
 
-            InputStream inputStream = socket.getInputStream();
-            BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(inputStream));
-            int read = bufferedReader.read();
-            while(read != -1){
-                System.out.print((char)read);
-                read = bufferedReader.read();
+            while(true){
+                StringBuilder wt = new StringBuilder();
+                InputStream syin = System.in;
+                int read = syin.read();
+                while(read!=10 &&read!=13){
+                    wt.append((char)read);
+                    read = syin.read();
+                }
+                OutputStream outputStream = socket.getOutputStream();
+                outputStream.write(wt.toString().getBytes());
+                outputStream.write(System.lineSeparator().getBytes());
+                outputStream.flush();
             }
         }catch (Exception e){
             e.printStackTrace();
