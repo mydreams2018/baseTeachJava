88179606918adaaae9d3f2efc388905c83414d73
kungreat <mydreams2018@outlook.com>
2021-10-10 17:23:27 +0800

    ByteBuffer-创建方式2

--- src/nio/NioTest.java
+++ src/nio/NioTest.java
@@ -1,6 +1,7 @@
 package nio;
 
 import java.io.*;
+import java.nio.ByteBuffer;
 
 public class NioTest {
 
@@ -8,7 +9,19 @@ public class NioTest {
 
 
     public static void main(String[] args) throws Exception {
+        ByteBuffer allocate = ByteBuffer.allocate(16);
+        System.out.println(allocate.position());//0
+        System.out.println(allocate.limit());//16
+        System.out.println(allocate.capacity());//16
 
+        ByteBuffer allocateDirect = ByteBuffer.allocateDirect(16);
+        System.out.println(allocateDirect.position());//0
+        System.out.println(allocateDirect.limit());//16
+        System.out.println(allocateDirect.capacity());//16
+
+        byte[] bts = {1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16};
+        ByteBuffer wrap = ByteBuffer.wrap(bts);
+        ByteBuffer wrap1 = ByteBuffer.wrap(bts, 0, 8);
 
     }
 }
