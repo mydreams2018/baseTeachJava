0fdcb2be92f99571a7d0c26470670bc29aa55710
kungreat <mydreams2018@outlook.com>
2021-11-07 10:21:34 +0800

    base 1

--- src/nio/NioTest.java
+++ src/nio/NioTest.java
@@ -15,6 +15,7 @@ public class NioTest {
     static File file = new File("D:\\temp\\src\\test.txt");
     static File fileOut = new File("D:\\temp\\src\\test2.txt");
 
+    static final ByteBuffer byteBuffer = ByteBuffer.allocate(1024);
     //server
     public static void main(String[] args) throws Exception {
         //ServerSocketChannel    SelectionKey.OP_ACCEPT
@@ -22,50 +23,19 @@ public class NioTest {
             Selector selector = Selector.open()){
             serverSocketChannel.configureBlocking(false);
             serverSocketChannel.bind(new InetSocketAddress(InetAddress.getLocalHost(),10086));
-
             serverSocketChannel.register(selector, SelectionKey.OP_ACCEPT);
-            ByteBuffer byteBuffer = ByteBuffer.allocate(1024);
+
             // 检查是否有 相应的通道已准备好用于 I/O 操作。
-            while(selector.select() > 0){
+            while(selector.select() >= 0){
                 Thread.sleep(1000);
                 Set<SelectionKey> selectionKeys = selector.selectedKeys();
                 Iterator<SelectionKey> iterator = selectionKeys.iterator();
                 System.out.println("size" + selectionKeys.size());
                 while(iterator.hasNext()){
                     SelectionKey next = iterator.next();
-                    SelectableChannel channel = next.channel();
                     iterator.remove();
-                    if(next.isValid() && next.isAcceptable()){
-                        ServerSocketChannel serChannel = (ServerSocketChannel) channel;
-                        SocketChannel accept = serChannel.accept();
-                        if(accept != null && accept.finishConnect()){
-                            System.out.println("accept");
-                            accept.configureBlocking(false);
-                            accept.register(selector,SelectionKey.OP_CONNECT|
-                                    SelectionKey.OP_READ|SelectionKey.OP_WRITE);
-                        }else{
-                            System.out.println("error");
-                        }
-                    }else if(next.isValid() && next.isConnectable()){
-                        System.out.println("connectable");
-                    }else if(next.isValid() && next.isReadable()){
-                        System.out.println("readable");
-                        SocketChannel clientChannel = (SocketChannel) channel;
-                        int read = clientChannel.read(byteBuffer);
-                        // read > 0 有数据  ==-1 表示流关闭  ==0 不管
-                        while(read > 0){
-                            System.out.println(new String(byteBuffer.array(),0,read));
-                            byteBuffer.clear();
-                            read = clientChannel.read(byteBuffer);
-                        }
-                        if(read == -1){
-                            clientChannel.close();
-                        }
-                    }else if(next.isValid() && next.isWritable()){
-                        System.out.println("writable");
-                    }else{
-                        System.out.println("error");
-                    }
+                    //可支持多线程操作
+                    runServer(next,selector);
                 }
             }
 
@@ -73,6 +43,44 @@ public class NioTest {
             e.printStackTrace();
         }
 
+    }
 
+    public static void runServer(SelectionKey next,Selector selector){
+        try {
+            SelectableChannel channel = next.channel();
+            if(next.isValid() && next.isAcceptable()){
+                ServerSocketChannel serChannel = (ServerSocketChannel) channel;
+                SocketChannel accept = serChannel.accept();
+                if(accept != null && accept.finishConnect()){
+                    System.out.println("accept");
+                    accept.configureBlocking(false);
+                    accept.register(selector,SelectionKey.OP_CONNECT|
+                            SelectionKey.OP_READ|SelectionKey.OP_WRITE);
+                }else{
+                    System.out.println("error");
+                }
+            }else if(next.isValid() && next.isConnectable()){
+                System.out.println("connectable");
+            }else if(next.isValid() && next.isReadable()){
+                System.out.println("readable");
+                SocketChannel clientChannel = (SocketChannel) channel;
+                int read = clientChannel.read(byteBuffer);
+                // read > 0 有数据  ==-1 表示流关闭  ==0 不管
+                while(read > 0){
+                    System.out.println(new String(byteBuffer.array(),0,read));
+                    byteBuffer.clear();
+                    read = clientChannel.read(byteBuffer);
+                }
+                if(read == -1){
+                    clientChannel.close();
+                }
+            }else if(next.isValid() && next.isWritable()){
+                System.out.println("writable");
+            }else{
+                System.out.println("error");
+            }
+        }catch (Exception e){
+            e.printStackTrace();
+        }
     }
 }
