897f45f3b3cf650b7384c116d1774453593855c5
kungreat <mydreams2018@outlook.com>
2021-11-14 12:46:44 +0800

    socket client

--- src/nio/NioClientTest.java
+++ src/nio/NioClientTest.java
@@ -5,9 +5,13 @@ import java.net.InetAddress;
 import java.net.InetSocketAddress;
 import java.net.StandardSocketOptions;
 import java.nio.ByteBuffer;
+import java.nio.channels.SelectionKey;
+import java.nio.channels.Selector;
 import java.nio.channels.ServerSocketChannel;
 import java.nio.channels.SocketChannel;
 import java.nio.file.Path;
+import java.util.Iterator;
+import java.util.Set;
 
 public class NioClientTest {
     static Path path = Path.of("D:\\temp\\src","test.txt");
@@ -16,15 +20,36 @@ public class NioClientTest {
 
     //client
     public static void main(String[] args) throws Exception {
-        SocketChannel socketChannel = SocketChannel.open();
-        System.out.println(socketChannel.isBlocking());//true
-//        socketChannel.bind(new InetSocketAddress(InetAddress.getLocalHost(),20077));
-        socketChannel.connect(new InetSocketAddress(InetAddress.getLocalHost(), 10086));
-        ByteBuffer byteBuffer = ByteBuffer.allocate(1024);
-        byteBuffer.put("client - hello word".getBytes());
-        byteBuffer.flip();
-        socketChannel.write(byteBuffer);
-
-        socketChannel.close();
+        try(SocketChannel socketChannel = SocketChannel.open();
+            Selector selector = Selector.open()){
+            socketChannel.configureBlocking(false);
+            socketChannel.bind(new InetSocketAddress(InetAddress.getLocalHost(),20077));
+            socketChannel.connect(new InetSocketAddress(InetAddress.getLocalHost(), 10086));
+            socketChannel.register(selector, SelectionKey.OP_CONNECT);
+            while(selector.select()>=0){
+                Thread.sleep(1000);
+                Set<SelectionKey> selectionKeys = selector.selectedKeys();
+                Iterator<SelectionKey> iterator = selectionKeys.iterator();
+                while(iterator.hasNext()){
+                    SelectionKey next = iterator.next();
+                    iterator.remove();
+                    SocketChannel clientChannel = (SocketChannel) next.channel();
+                    if(next.isValid() && next.isConnectable()){
+                        while(!clientChannel.finishConnect()){
+                        }
+                        clientChannel.register(selector,SelectionKey.OP_READ);
+                        System.out.println("connectable");
+                    }else if(next.isValid() && next.isReadable()){
+                        System.out.println("Readable");
+                    }else if(next.isValid() && next.isWritable()){
+                        System.out.println("Writable");
+                    }else{
+                        System.out.println("error");
+                    }
+                }
+            }
+        }catch (Exception e){
+            e.printStackTrace();
+        }
     }
 }
