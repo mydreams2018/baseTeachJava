6bdcb6d5f6818fddef9b83e099d9e5b66621e274
kungreat <mydreams2018@outlook.com>
2021-12-24 22:07:43 +0800

    base

--- src/jarts/ClassLoadTs.java
+++ src/jarts/ClassLoadTs.java
@@ -18,7 +18,11 @@ public class ClassLoadTs {
         }
     }
 /*
+ [硬编码:例导包那种、直接引用那种、需要在加载运行链接时就指定资源]
 * 反射框架核心、通过指定ClassLoader指定加载必要资源、再通过反射去启动加载别的模块完成整个功能、
+* A-->系统ClassLoad-->平台ClassLoader
+* B-->系统ClassLoad-->平台ClassLoader
+* 对象概念 A、B各自管各自的资源
 */
     public static void main(String[] args) throws Exception {
         MyURLClassLoader urlClassLoader = new MyURLClassLoader(new URL[]{url});
