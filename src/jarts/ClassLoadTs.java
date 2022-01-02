d6450b4d6854d92fb835723f995039b0cb2e1c40
kungreat <mydreams2018@outlook.com>
2022-01-02 10:40:59 +0800

    反射框架核心

--- src/jarts/ClassLoadTs.java
+++ src/jarts/ClassLoadTs.java
@@ -7,11 +7,9 @@ import java.net.URL;
 public class ClassLoadTs {
     static URL url;
     static URL url2;
-    static URL url3;
     static {
         try {
-            url3 = new URL("file:C:\\Users\\kungreat\\IdeaProjects\\mavenTest\\target\\classes\\cn\\kungreat\\ts\\Mv.class");
-            url2 = new URL("file:C:\\Users\\kungreat\\IdeaProjects\\mavenTest\\target\\classes\\cn\\kungreat\\MavenTest.class");
+            url2 = new URL("file:C:\\Users\\kungreat\\IdeaProjects\\mavenTest\\target\\classes\\");
             url = new URL("file:C:\\Users\\kungreat\\.m2\\repository\\commons-codec\\commons-codec\\1.11\\commons-codec-1.11.jar");
         } catch (MalformedURLException e) {
             e.printStackTrace();
@@ -25,9 +23,9 @@ public class ClassLoadTs {
 * 对象概念 A、B各自管各自的资源
 */
     public static void main(String[] args) throws Exception {
-        MyURLClassLoader urlClassLoader = new MyURLClassLoader(new URL[]{url});
-        Class<?> aClass = urlClassLoader.fileLoad(url2, "cn.kungreat.MavenTest");
-        urlClassLoader.fileLoad(url3, "cn.kungreat.ts.Mv");
+        MyURLClassLoader urlClassLoader = new MyURLClassLoader(new URL[]{url,url2});
+        urlClassLoader.setDefaultAssertionStatus(false);
+        Class<?> aClass = urlClassLoader.loadClass("cn.kungreat.MavenTest");
         Method ma = aClass.getDeclaredMethod("main", String[].class);
         ma.invoke(null,(Object) null);
     }
