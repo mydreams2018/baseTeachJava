e30013a5a72043db880ca07d6081530823ee63ce
kungreat <mydreams2018@outlook.com>
2022-01-09 21:53:56 +0800

    lambda stream 介绍

--- src/java8/Apple.java
+++ src/java8/Apple.java
@@ -1,11 +1,24 @@
 package java8;
 
 public class Apple {
+    private long num = 0;
     public Apple(){
         System.out.println("apple");
     }
 
     public Apple(Integer num){
         System.out.println(num);
+    }
+
+    public void add(long num){
+        this.num = this.num + num;
+    }
+
+    public long getNum() {
+        return num;
+    }
+
+    public void setNum(long num) {
+        this.num = num;
     }
 }
--- src/java8/LambdaTs2.java
+++ src/java8/LambdaTs2.java
@@ -1,24 +1,27 @@
 package java8;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.util.*;
 import java.util.function.Function;
 import java.util.function.Predicate;
 import java.util.function.Supplier;
+import java.util.stream.Collectors;
+import java.util.stream.LongStream;
+import java.util.stream.Stream;
 
 public class LambdaTs2 {
 
     static List<Integer> list = new ArrayList<>();
    static {
-        list.add(1554);
+        list.add(1554);//1
         list.add(452);
-        list.add(54546);
+        list.add(54546);//1
         list.add(81);
         list.add(651);
+        list.add(81);
         list.add(895);
         list.add(356);
         list.add(785);
-        list.add(3485);
+        list.add(3485); // 上一次的结果 +
     }
 
     public static void main(String[] args){
@@ -35,13 +38,52 @@ public class LambdaTs2 {
 //        Supplier<Apple> c2 = () -> { return new Apple();};
 //        Apple a2 = c2.get();
 
-        Function<Integer,Apple[]> c2 = Apple[]::new;
-        Apple[] a2 = c2.apply(16);
-        System.out.println(a2.length);
+//        Function<Integer,Apple[]> c2 = Apple[]::new;
+//        Apple[] a2 = c2.apply(16);
+//        System.out.println(a2.length);
+//
+//        Function<Integer, Apple[]> c3 = (weight) -> new Apple[weight];
+//        Apple[] a3 = c3.apply(110);
+//        System.out.println(a3.length);
 
-        Function<Integer, Apple[]> c3 = (weight) -> new Apple[weight];
-        Apple[] a3 = c3.apply(110);
-        System.out.println(a3.length);
+//        int sum = list.stream().filter(b -> b > 1000).mapToInt(b -> b).sum();
+//        //多线程并发
+//        int parallelSum = list.parallelStream().filter(b -> b > 1000).mapToInt(b -> b).sum();
+//        System.out.println(sum);
+//        System.out.println(parallelSum);
+//        List<String> l = new ArrayList(Arrays.asList("one", "two"));
+//        Stream<String> sl = l.stream();
+//        l.add("three");
+//        String s = sl.collect(Collectors.joining(","));
+//        System.out.println(s);
+
+//        List<Integer> results = list.stream().filter(s -> s > 1000).collect(Collectors.toList());
+//        System.out.println(results);
+
+//        Optional<Integer> reduce = list.stream().filter(s -> s > 1000).reduce((x, y) -> {
+//            System.out.println(x+":"+y);
+//            return x + y;});
+//        System.out.println(reduce.isPresent());
+//        System.out.println(reduce.get());
+
+        //这些归约操作可以安全地并行运行，几乎不需要修改： ForkJoinPool  1000->filter -->reduce   [N]+[N]
+//        Optional<Integer> reduce = list.parallelStream().filter(s -> s > 1000).reduce((x, y) -> x + y);
+//        System.out.println(reduce.isPresent());
+//        System.out.println(reduce.get());
+
+//        OptionalInt heaviest = list.parallelStream().mapToInt(s -> s).min();
+//        System.out.println(heaviest.getAsInt());
+
+//        Stream<Integer> limit = list.stream().distinct();
+//        limit.forEach(System.out::println);
+//        long count = list.stream().distinct().count();
+//        System.out.println(count);
+
+        Apple apple = new Apple();
+        //线程不安全的方式
+        LongStream.rangeClosed(1,9999999L).parallel().forEach(apple::add);
+//-5958941808141140780  6684975474523669191
+        System.out.println(apple.getNum());
     }
 
     public boolean ts(String s){
